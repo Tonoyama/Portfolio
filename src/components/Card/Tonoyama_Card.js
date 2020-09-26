cba17580920a71c2a5d2c6265235d99bf132a061
Tonoyama <yudaitonoyama@gmail.com>
2020-09-26 21:11:57 +0900

    Compiled Successfully!

--- src/components/Card/Tonoyama_Card.js
+++ src/components/Card/Tonoyama_Card.js
@@ -2,27 +2,55 @@ import React from 'react';
 import '../assets/styles/Card.css'
 import Clock from '../Clock.js'
 import tonoyama from '../assets/images/tonoyama.jpg'
+import MediaQuery from 'react-responsive'
 
 class Card extends React.Component {
     render() {
         return (
-            <div className="contents">
-                <div className="Sidenav">
-                    <img className="Cardimg" src={tonoyama} alt="" />
-                    <div>
-                        <span className="Bold-font">I'm Tonoyama</span><span className="Light-font"> yudai</span>
-                    </div>
-                </div>
-                <div className="Comment">
-                    <p>今日も１日頑張るぞい╭( ･ㅂ･)و</p>
-                    <br />
-                    <p>殿山 雄大と申します。大阪のとある大学の情報工学科 学部１年です。</p>
-                    <p>クラウドコンピューティング、Web 開発、機械学習に興味があります。</p>
-                    <p>
-                        <Clock></Clock> 現在、京都のWeb系自社開発企業でアルバイトしていて、
+            <div className="App">
+                <MediaQuery query="(max-width: 780px)">
+                    <div className="Contents">
+                        <div className="Sidenav">
+                            <img className="Cardimg" src={tonoyama} alt="" />
+                            <div>
+                                <span className="Bold-font">I'm Tonoyama</span><span className="Light-font"> yudai</span>
+                            </div>
+                        </div>
+                        <div className="Comment">
+                            <p>今日も１日頑張るぞい╭( ･ㅂ･)و</p>
+                            <br />
+                            <p>殿山 雄大と申します。大阪のとある大学の情報工学科 学部１年です。</p>
+                            <p>クラウドコンピューティング、Web 開発、機械学習に興味があります。</p>
+                            <p>
+                                <Clock></Clock> 現在、京都のWeb系自社開発企業でアルバイトしていて、
                         主に JavaScript を使った開発やテストを行っています。
                     </p>
-                </div>
+                        </div>
+                    </div>
+                </MediaQuery>
+                <MediaQuery query="(min-width: 781px)">
+                    <div className="PC_Contents">
+                        <div className="PC_Sidenav">
+                            <img className="Cardimg" src={tonoyama} alt="" />
+                            <div>
+                                <span className="Bold-font">I'm Tonoyama</span><span className="Light-font"> yudai</span>
+                            </div>
+                        </div>
+                        <div className="PC_title">
+                            {this.props.title}
+                        </div>
+                        <div className="PC_Comment">
+                            <p>今日も１日頑張るぞい╭( ･ㅂ･)و</p>
+                            <br />
+                            <p>殿山 雄大と申します。大阪のとある大学の情報工学科 学部１年です。</p>
+                            <p>クラウドコンピューティング、Web 開発、機械学習に興味があります。</p>
+                            <p>
+                                <Clock></Clock> 現在、京都のWeb系自社開発企業でアルバイトしていて、
+                        主に JavaScript を使った開発やテストを行っています。
+                    </p>
+                        </div>
+                    </div>
+                </MediaQuery>
             </div>
         )
     }
--- src/components/Tonoyama.js
+++ src/components/Tonoyama.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card/Card.js'
+import Card from './Card/Tonoyama_Card.js'
 import Menu from './Menu.js'
 import tonoyama from './assets/images/tonoyama.jpg'
 import MediaQuery from 'react-responsive'
@@ -10,12 +10,10 @@ const Tonoyama = () => {
         <div className="Body">
             <MediaQuery query="(max-width: 780px)">
                 <Menu />
-                <Card img={tonoyama} boldfont="I'm Tonoyama" lightfont="yudai" title="今日も１日頑張るぞい╭( ･ㅂ･)وｸﾞｯ" comment="殿山 雄大と申します。大阪のとある大学の情報工学科 学部１年です。クラウドコンピューティング、Web 開発、機械学習に興味があります。現在、京都のWeb系自社開発企業でアルバイトしていて、
-                        主に JavaScript を使った開発やテストを行っています。" />
+                <Card />
             </MediaQuery>
             <MediaQuery query="(min-width: 779px)">
-                <Card img={tonoyama} boldfont="I'm Tonoyama" lightfont="yudai" title="今日も１日頑張るぞい╭( ･ㅂ･)وｸﾞｯ" comment="殿山 雄大と申します。大阪のとある大学の情報工学科 学部１年です。クラウドコンピューティング、Web 開発、機械学習に興味があります。現在、京都のWeb系自社開発企業でアルバイトしていて、
-                        主に JavaScript を使った開発やテストを行っています。" />
+                <Card />
             </MediaQuery>
         </div>
     );
